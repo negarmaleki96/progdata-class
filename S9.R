43803c2e37ff7c46630befcaf614b462acecb426
Negar <78661548+negarmaleki96@users.noreply.github.com>
2022-02-08 11:48:42 -0500

    S9

--- S9.R
+++ S9.R
@@ -28,3 +28,41 @@ dat_bfi |>
       na.rm = TRUE
     )
   )
+
+dat_bfi |>
+  group_by(gender) |>
+  summarize(
+    across(
+      A1:A5,
+      \(x) mean(x, na.rm = TRUE)
+    )
+  )
+
+dat_bfi |>
+  group_by(gender) |>
+  summarize(
+    across(
+      A1:A5,
+      list(
+        mean = \(x) mean(x, na.rm = TRUE),
+        sd = \(x) sd(x, na.rm = TRUE)
+      )
+    )
+  )
+
+dat_bfi |>
+  mutate(
+    A1r = recode(
+      A1, 
+      "6" = 1, "5" = 2, "4" = 3, "3" = 4, "2" = 5, "1" = 6
+    )
+  ) |>
+  select(A1, A1r) |> 
+  head()
+
+# or
+
+dat_bfi |>
+  mutate(A1r = max(A1, na.rm = TRUE) - A1 + min(A1, na.rm = TRUE)) |>
+  select(A1, A1r) |> 
+  head()
